19d24d985d7bfba5560a045767b11105fc9d76de
mdouradoo <maria_eduardaa_dourado@yahoo.com>
2025-09-19 11:05:11 -0300

    atualizacao

--- gerenciamento/gerenciamento/Form1.Designer.cs
+++ gerenciamento/gerenciamento/Form1.Designer.cs
@@ -29,12 +29,303 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtdescricao = new System.Windows.Forms.TextBox();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.txtpesquisar = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.chkpagamento = new System.Windows.Forms.DateTimePicker();
+            this.btncadastrar = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btnexcluir = new System.Windows.Forms.Button();
+            this.btnproximo = new System.Windows.Forms.Button();
+            this.btnpesquisar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbotipo = new System.Windows.Forms.ComboBox();
+            this.cboservico = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(155, 106);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(177, 22);
+            this.txtcodigo.TabIndex = 0;
+            this.txtcodigo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtdescricao
+            // 
+            this.txtdescricao.Location = new System.Drawing.Point(155, 160);
+            this.txtdescricao.Name = "txtdescricao";
+            this.txtdescricao.Size = new System.Drawing.Size(177, 22);
+            this.txtdescricao.TabIndex = 0;
+            this.txtdescricao.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(155, 216);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(177, 22);
+            this.txtvalor.TabIndex = 0;
+            this.txtvalor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtpesquisar
+            // 
+            this.txtpesquisar.Location = new System.Drawing.Point(815, 139);
+            this.txtpesquisar.Name = "txtpesquisar";
+            this.txtpesquisar.Size = new System.Drawing.Size(290, 22);
+            this.txtpesquisar.TabIndex = 0;
+            this.txtpesquisar.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(155, 443);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // chkpagamento
+            // 
+            this.chkpagamento.Location = new System.Drawing.Point(155, 385);
+            this.chkpagamento.Name = "chkpagamento";
+            this.chkpagamento.Size = new System.Drawing.Size(200, 22);
+            this.chkpagamento.TabIndex = 3;
+            this.chkpagamento.ValueChanged += new System.EventHandler(this.chkpagamento_ValueChanged);
+            // 
+            // btncadastrar
+            // 
+            this.btncadastrar.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btncadastrar.Location = new System.Drawing.Point(155, 512);
+            this.btncadastrar.Name = "btncadastrar";
+            this.btncadastrar.Size = new System.Drawing.Size(95, 31);
+            this.btncadastrar.TabIndex = 4;
+            this.btncadastrar.Text = "Cadastrar";
+            this.btncadastrar.UseVisualStyleBackColor = false;
+            this.btncadastrar.Click += new System.EventHandler(this.btncadastrar_Click);
+            // 
+            // btneditar
+            // 
+            this.btneditar.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btneditar.Location = new System.Drawing.Point(272, 512);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(83, 31);
+            this.btneditar.TabIndex = 4;
+            this.btneditar.Text = "Editar";
+            this.btneditar.UseVisualStyleBackColor = false;
+            this.btneditar.Click += new System.EventHandler(this.btneditar_Click);
+            // 
+            // btnexcluir
+            // 
+            this.btnexcluir.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnexcluir.Location = new System.Drawing.Point(381, 512);
+            this.btnexcluir.Name = "btnexcluir";
+            this.btnexcluir.Size = new System.Drawing.Size(78, 31);
+            this.btnexcluir.TabIndex = 4;
+            this.btnexcluir.Text = "Excluir";
+            this.btnexcluir.UseVisualStyleBackColor = false;
+            this.btnexcluir.Click += new System.EventHandler(this.btnexcluir_Click);
+            // 
+            // btnproximo
+            // 
+            this.btnproximo.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnproximo.Location = new System.Drawing.Point(1125, 620);
+            this.btnproximo.Name = "btnproximo";
+            this.btnproximo.Size = new System.Drawing.Size(105, 49);
+            this.btnproximo.TabIndex = 4;
+            this.btnproximo.Text = "Próximo";
+            this.btnproximo.UseVisualStyleBackColor = false;
+            // 
+            // btnpesquisar
+            // 
+            this.btnpesquisar.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnpesquisar.Location = new System.Drawing.Point(1125, 139);
+            this.btnpesquisar.Name = "btnpesquisar";
+            this.btnpesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnpesquisar.TabIndex = 4;
+            this.btnpesquisar.Text = "Ok";
+            this.btnpesquisar.UseVisualStyleBackColor = false;
+            this.btnpesquisar.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Descrição";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Tipo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 336);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Serviço";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(812, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Pesquisar";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // cbotipo
+            // 
+            this.cbotipo.FormattingEnabled = true;
+            this.cbotipo.Location = new System.Drawing.Point(155, 276);
+            this.cbotipo.Name = "cbotipo";
+            this.cbotipo.Size = new System.Drawing.Size(121, 24);
+            this.cbotipo.TabIndex = 6;
+            // 
+            // cboservico
+            // 
+            this.cboservico.FormattingEnabled = true;
+            this.cboservico.Location = new System.Drawing.Point(155, 328);
+            this.cboservico.Name = "cboservico";
+            this.cboservico.Size = new System.Drawing.Size(121, 24);
+            this.cboservico.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.MidnightBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(815, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(385, 234);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(62, 390);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 16);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Data";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(62, 443);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 16);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Pagamento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1291, 710);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cboservico);
+            this.Controls.Add(this.cbotipo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnpesquisar);
+            this.Controls.Add(this.btnproximo);
+            this.Controls.Add(this.btnexcluir);
+            this.Controls.Add(this.btneditar);
+            this.Controls.Add(this.btncadastrar);
+            this.Controls.Add(this.chkpagamento);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtpesquisar);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.txtdescricao);
+            this.Controls.Add(this.txtcodigo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtdescricao;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.TextBox txtpesquisar;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DateTimePicker chkpagamento;
+        private System.Windows.Forms.Button btncadastrar;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btnexcluir;
+        private System.Windows.Forms.Button btnproximo;
+        private System.Windows.Forms.Button btnpesquisar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbotipo;
+        private System.Windows.Forms.ComboBox cboservico;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- gerenciamento/gerenciamento/Form1.cs
+++ gerenciamento/gerenciamento/Form1.cs
@@ -16,5 +16,92 @@ namespace gerenciamento
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btneditar_Click(object sender, EventArgs e)
+        {
+            //chamo o metodo da conexao
+            conexao com = new conexao();
+            com.getConexao();
+            // chama o objeto do financeiro
+            financeirocs financeiro = new financeirocs();
+            financeiro.id = Convert.ToInt32(txtCodigo.Text);
+            financeiro.descricao = txtDescricao.Text;
+            financeiro.servico = cboServico.Text;
+            financeiro.tipo = cboTipo.Text;
+            financeiro.valor = decimal.Parse(txtValor.Text);
+            financeiro.pgto = chkpagamento.Checked;
+            financeiro.data_lancamento = Data_lancamento.Value;
+            if (financeiro.editar(com) == true)
+            {
+                MessageBox.Show("Editado com sucesso!");
+            }
+        }
+
+        private void btnexcluir_Click(object sender, EventArgs e)
+        {
+            //chamo o metodo da conexao
+            conexao com = new conexao();
+            com.getConexao();
+            // chama o objeto do financeiro
+            financeirocs financeiro = new financeirocs();
+            financeiro.id = Convert.ToInt32(txtCodigo.Text);
+            if (financeiro.Excluir(com) == true)
+            {
+                MessageBox.Show("Excluido com sucesso");
+                dataGridView1.Refresh();
+            }
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            conexao com = new conexao();
+            com.getConexao();
+            if (string.IsNullOrEmpty(txtPesquisar.Text))
+            {
+                dataGridView1.DataSource = com.obterdados("select * from financeiro");
+            }
+            else
+            {
+                dataGridView1.DataSource = com.obterdados("select * from financeiro where descricao like '%" + txtPesquisar.Text + "%' or data_lancamento like '%" + txtPesquisar.Text + "%'");
+            }
+        }
+
+        private void btncadastrar_Click(object sender, EventArgs e)
+        {
+            conexao con = new conexao();
+            con.getConexao();
+            //chamando o objeto financeiro
+            financeirocs fin = new financeirocs();
+            //populando as informações
+            fin.data_lancamento = Data_lancamento.Value;
+            fin.descricao = txtDescricao.Text;
+            fin.servico = cboServico.Text;
+            fin.valor = decimal.Parse(txtValor.Text);
+            fin.tipo = cboTipo.Text;
+            fin.pgto = chkpagamento.Checked;
+            if (fin.cadastrar(con) == true)
+            {
+                MessageBox.Show("Cadastrado com sucesso");
+                dataGridView1.Refresh();// atualiza o grid
+            }
+        }
+
+        private void chkpagamento_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+        }
     }
 }
